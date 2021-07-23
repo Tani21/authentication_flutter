984a82d6749ded9bb1241d7fd5d33428bbab4d84
Tanishka Vaswani <tanishka@pop-os.localdomain>
2021-07-23 22:57:03 +0530

    anonymous signin

---
+++ android/app/src/main/kotlin/com/example/authentication_as/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authentication_as
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  var firestore = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(onPressed: () async {
+              main();
+      UserCredential userCredential = await auth.signInAnonymously();
+      print(userCredential);
+      }, child: Text('Anonymous')
+
+        ),
+
+
+      ],
+      ),
+      ),
+
+    );
+  }
+}
